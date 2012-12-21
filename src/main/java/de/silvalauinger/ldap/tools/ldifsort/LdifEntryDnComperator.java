11d0a305f38e69d948b355bd7213fda9ad9282c8
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2012-12-21 06:23:16 +0100

    Implemented an LdifEntryDnComperator that compares LdifEntries hierarchically on their DNs.

---
+++ src/main/java/de/silvalauinger/ldap/tools/ldifsort/LdifEntryDnComperator.java
@@ -0,0 +1,26 @@
+package de.silvalauinger.ldap.tools.ldifsort;
+
+import com.google.common.collect.Ordering;
+import java.util.Comparator;
+import org.apache.directory.shared.ldap.model.ldif.LdifEntry;
+import org.apache.directory.shared.ldap.model.name.Dn;
+
+public final class LdifEntryDnComperator implements Comparator<LdifEntry> {
+
+    @Override
+    public int compare(final LdifEntry left, final LdifEntry right) {
+	final Dn leftDn = left.getDn();
+	final Dn rightDn = right.getDn();
+	if (leftDn.isAncestorOf(rightDn)) {
+	    return -1;
+	} else if (leftDn.isDescendantOf(rightDn)) {
+	    return 1;
+	} else if (leftDn.size() < rightDn.size()) {
+	    return -1;
+	} else if (leftDn.size() > rightDn.size()) {
+	    return 1;
+	} else {
+	    return Ordering.usingToString().compare(left, right);
+	}
+    }
+}
