bea985c618657aaeade8e44dfcb34b63694ba86c
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2012-12-21 03:20:07 +0100

    Implemented PathParser for parsing file paths from the CLI.

---
+++ src/main/java/de/silvalauinger/ldap/tools/ldifsort/cli/parse/PathParser.java
@@ -0,0 +1,19 @@
+package de.silvalauinger.ldap.tools.ldifsort.cli.parse;
+
+import com.martiansoftware.jsap.ParseException;
+import com.martiansoftware.jsap.StringParser;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class PathParser extends StringParser {
+
+    @Override
+    public Path parse(final String string) throws ParseException {
+	try {
+	    return Paths.get(string);
+	} catch (final IllegalArgumentException | FileSystemNotFoundException | SecurityException exception) {
+	    throw new ParseException(exception);
+	}
+    }
+}
