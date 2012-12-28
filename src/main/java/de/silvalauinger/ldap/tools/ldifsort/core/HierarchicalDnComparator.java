479758e11ea4ab60192da0b6ce7a2a878b057156
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2012-12-28 17:39:29 +0100

    Wrote some tests for HierarchicalDnComparator and fixed the class.

--- src/main/java/de/silvalauinger/ldap/tools/ldifsort/core/HierarchicalDnComparator.java
+++ src/main/java/de/silvalauinger/ldap/tools/ldifsort/core/HierarchicalDnComparator.java
@@ -1,23 +1,26 @@
 package de.silvalauinger.ldap.tools.ldifsort.core;
 
 import com.google.common.collect.Ordering;
+import static java.lang.Integer.signum;
 import java.util.Comparator;
 import org.apache.directory.shared.ldap.model.name.Dn;
 
-public class HierarchicalDnComparator implements Comparator<Dn> {
+public final class HierarchicalDnComparator implements Comparator<Dn> {
 
     @Override
     public int compare(final Dn left, final Dn right) {
-	if (left.isAncestorOf(right)) {
-	    return -1;
-	} else if (left.isDescendantOf(right)) {
-	    return 1;
-	} else if (left.size() < right.size()) {
-	    return -1;
-	} else if (left.size() > right.size()) {
-	    return 1;
-	} else {
-	    return Ordering.usingToString().compare(left, right);
-	}
+        if (left.equals(right)) {
+            return 0;
+        } else if (left.isAncestorOf(right)) {
+            return -1;
+        } else if (left.isDescendantOf(right)) {
+            return 1;
+        } else if (left.size() < right.size()) {
+            return -1;
+        } else if (left.size() > right.size()) {
+            return 1;
+        } else {
+            return signum(Ordering.usingToString().compare(left, right));
+        }
     }
 }
