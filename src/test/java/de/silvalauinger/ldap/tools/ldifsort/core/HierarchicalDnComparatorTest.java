c421c12c0f8fb08cc8edfb33ee5f065c801ef620
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2012-12-29 00:29:34 +0100

    HierarchicalDnComparatorTest: small cleanups

--- src/test/java/de/silvalauinger/ldap/tools/ldifsort/core/HierarchicalDnComparatorTest.java
+++ src/test/java/de/silvalauinger/ldap/tools/ldifsort/core/HierarchicalDnComparatorTest.java
@@ -1,8 +1,6 @@
 package de.silvalauinger.ldap.tools.ldifsort.core;
 
-import com.google.common.collect.Lists;
 import static com.google.common.collect.Lists.newArrayList;
-import com.google.common.collect.Ordering;
 import java.util.List;
 import org.apache.directory.shared.ldap.model.exception.LdapInvalidDnException;
 import org.apache.directory.shared.ldap.model.name.Dn;
@@ -17,39 +15,39 @@ public class HierarchicalDnComparatorTest {
 
     private final Dn left;
     private final Dn right;
-    private final int compareExpectedResult;
+    private final int expectedCompareResult;
 
     public HierarchicalDnComparatorTest(final String leftDn, final String rightDn, final int compareExpectedResult) throws LdapInvalidDnException {
-        this.left = new Dn(leftDn);
-        this.right = new Dn(rightDn);
-        this.compareExpectedResult = compareExpectedResult;
+	this.left = new Dn(leftDn);
+	this.right = new Dn(rightDn);
+	this.expectedCompareResult = compareExpectedResult;
     }
 
     @Parameters
     public static List<Object[]> data() {
-        return newArrayList(
-                /* compare(x, x) == 0 */
-                new Object[]{"dn=x", "dn=x", 0},
-                /* sgn(compare(x, y)) == -sgn(compare(x, y)) */
-                new Object[]{"dn=x", "dn=y", -1},
-                new Object[]{"dn=y", "dn=x", 1},
-                /* ((compare(x, y) > 0) && (compare(y, z) > 0)) => compare(x, z) > 0 */
-                new Object[]{"dn=z", "dn=y", 1},
-                new Object[]{"dn=y", "dn=x", 1},
-                new Object[]{"dn=z", "dn=y", 1},
-                /* more rdn's are bigger than less rdns */
-                new Object[]{"dn=y, dn=y", "dn=x", 1},
-                /* same base rdn, but different end rdn */
-                new Object[]{"dn=x, dn=x", "dn=x, dn=y", -1},
-                new Object[]{"dn=x, dn=y", "dn=x, dn=x", 1},
-                /* dn with more rdns, but same base is smaller */
-                new Object[]{"dn=x, dn=y, dn=z", "dn=x,dn=y", 1},
-                /* dn with less rdns, but same base is bigger */
-                new Object[]{"dn=x, dn=y", "dn=x,dn=y, dn=z", -1});
+	return newArrayList(
+		/* compare(x, x) == 0 */
+		new Object[]{"dn=x", "dn=x", 0},
+		/* sgn(compare(x, y)) == -sgn(compare(x, y)) */
+		new Object[]{"dn=x", "dn=y", -1},
+		new Object[]{"dn=y", "dn=x", 1},
+		/* ((compare(x, y) > 0) && (compare(y, z) > 0)) => compare(x, z) > 0 */
+		new Object[]{"dn=z", "dn=y", 1},
+		new Object[]{"dn=y", "dn=x", 1},
+		new Object[]{"dn=z", "dn=y", 1},
+		/* more rdn's are bigger than less rdns */
+		new Object[]{"dn=y, dn=y", "dn=x", 1},
+		/* same base rdn, but different end rdn */
+		new Object[]{"dn=x, dn=x", "dn=x, dn=y", -1},
+		new Object[]{"dn=x, dn=y", "dn=x, dn=x", 1},
+		/* dn with more rdns, but same base is smaller */
+		new Object[]{"dn=x, dn=y, dn=z", "dn=x,dn=y", 1},
+		/* dn with less rdns, but same base is bigger */
+		new Object[]{"dn=x, dn=y", "dn=x,dn=y, dn=z", -1});
     }
 
     @Test
     public void testCompare() {
-        assertEquals(compareExpectedResult, HierarchicalDnComparator.INSTANCE.compare(left, right));
+	assertEquals(expectedCompareResult, HierarchicalDnComparator.INSTANCE.compare(left, right));
     }
 }
