c32a5d056b70e72467f9cc366f5742da1621ea2f
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2012-12-29 02:48:49 +0100

    LdifSortCommand: some small improvements.

--- src/main/java/de/silvalauinger/ldap/tools/ldifsort/command/LdifSortCommand.java
+++ src/main/java/de/silvalauinger/ldap/tools/ldifsort/command/LdifSortCommand.java
@@ -1,34 +1,27 @@
 package de.silvalauinger.ldap.tools.ldifsort.command;
 
-import com.google.common.base.Function;
 import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.ImmutableList;
-import static com.google.common.collect.Ordering.from;
+import com.google.common.collect.Ordering;
 import de.silvalauinger.common.command.Command;
 import de.silvalauinger.common.command.CommandException;
-import de.silvalauinger.ldap.tools.ldifsort.core.HierarchicalDnComparator;
+import de.silvalauinger.ldap.tools.ldifsort.core.LdifEntryHierarchicalDnComparator;
 import org.apache.directory.shared.ldap.model.ldif.LdifEntry;
-import org.apache.directory.shared.ldap.model.name.Dn;
 
 public class LdifSortCommand implements Command<ImmutableList<LdifEntry>> {
 
     //<editor-fold defaultstate="collapsed" desc="attributes">
-    private final Iterable<LdifEntry> ldifToSort;
+    private final Iterable<LdifEntry> ldifEntries;
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="constructors">
-    public LdifSortCommand(final Iterable<LdifEntry> ldifToSort) {
-	this.ldifToSort = checkNotNull(ldifToSort);
+    public LdifSortCommand(final Iterable<LdifEntry> ldifEntries) {
+	this.ldifEntries = checkNotNull(ldifEntries);
     }
     //</editor-fold>
 
     @Override
     public ImmutableList<LdifEntry> execute() throws CommandException {
-	return from(HierarchicalDnComparator.INSTANCE).onResultOf(new Function<LdifEntry, Dn>() {
-	    @Override
-	    public Dn apply(final LdifEntry toSort) {
-		return toSort.getDn();
-	    }
-	}).immutableSortedCopy(ldifToSort);
+	return Ordering.from(LdifEntryHierarchicalDnComparator.INSTANCE).immutableSortedCopy(ldifEntries);
     }
 }
