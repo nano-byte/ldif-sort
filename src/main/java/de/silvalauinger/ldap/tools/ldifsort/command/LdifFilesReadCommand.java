843d4faba949d8ae5f1e60b17b44ac1139fae990
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2012-12-29 02:45:14 +0100

    Implemented LdifFilesReadCommand that reads multiple ldif files.

---
+++ src/main/java/de/silvalauinger/ldap/tools/ldifsort/command/LdifFilesReadCommand.java
@@ -0,0 +1,37 @@
+package de.silvalauinger.ldap.tools.ldifsort.command;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import de.silvalauinger.common.command.Command;
+import de.silvalauinger.common.command.CommandException;
+import de.silvalauinger.ldap.tools.ldifsort.core.LdifFilesReader;
+import java.io.File;
+import java.util.Iterator;
+import org.apache.directory.shared.ldap.model.ldif.LdapLdifException;
+import org.apache.directory.shared.ldap.model.ldif.LdifEntry;
+
+public class LdifFilesReadCommand implements Command<Iterable<LdifEntry>>, Iterable<LdifEntry> {
+
+    //<editor-fold defaultstate="collapsed" desc="attributes">
+    private final Iterable<File> ldifFiles;
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="constructors">
+    public LdifFilesReadCommand(final Iterable<File> ldifFiles) {
+	this.ldifFiles = checkNotNull(ldifFiles);
+    }
+    //</editor-fold>
+
+    @Override
+    public Iterable<LdifEntry> execute() throws CommandException {
+	try {
+	    return new LdifFilesReader(ldifFiles);
+	} catch (final LdapLdifException exception) {
+	    throw new CommandException(exception.getLocalizedMessage());
+	}
+    }
+
+    @Override
+    public Iterator<LdifEntry> iterator() {
+	return execute().iterator();
+    }
+}
