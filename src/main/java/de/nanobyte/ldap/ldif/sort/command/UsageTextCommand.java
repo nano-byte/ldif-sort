1462f39e55583d3e10bd89f41f0fd383c2889237
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2013-05-17 15:12:40 +0200

    Usage now only shows parameters.

--- src/main/java/de/nanobyte/ldap/ldif/sort/command/UsageTextCommand.java
+++ src/main/java/de/nanobyte/ldap/ldif/sort/command/UsageTextCommand.java
@@ -7,22 +7,18 @@ import de.nanobyte.common.command.CommandException;
 
 public class UsageTextCommand implements Command<String> {
 
-    //<editor-fold defaultstate="collapsed" desc="statics">
-    private final static String programName = "ldifsort";
-    private final static String programInvocation = "java -jar " + programName + ".jar";
-    //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="attributes">
     private final String usageText;
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="constructors">
     public UsageTextCommand(final JSAP cliParser) {
-	this.usageText = "Usage: " + programInvocation + " " + checkNotNull(cliParser).getUsage();
+        this.usageText = "Usage: " + checkNotNull(cliParser).getUsage();
     }
     //</editor-fold>
 
     @Override
     public String execute() throws CommandException {
-	return usageText;
+        return usageText;
     }
 }
