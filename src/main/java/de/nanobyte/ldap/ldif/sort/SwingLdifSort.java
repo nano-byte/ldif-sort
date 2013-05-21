0790a8067828e1b4a22d33d491ded65fc724d9e6
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2013-05-21 23:10:21 +0200

    Implemented new entry point to support a swing version of ldif sort.

---
+++ src/main/java/de/nanobyte/ldap/ldif/sort/SwingLdifSort.java
@@ -0,0 +1,10 @@
+package de.nanobyte.ldap.ldif.sort;
+
+import javax.swing.JFrame;
+
+public final class SwingLdifSort extends JFrame {
+
+    public SwingLdifSort(final String[] arguments) {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
