fee8d78b47875c9ed036252b90ee5c0920060477
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2013-05-23 17:48:22 +0200

    JList with ldif files now resizes correct.

--- src/main/java/de/nanobyte/ldap/ldif/sort/SwingLdifSort.java
+++ src/main/java/de/nanobyte/ldap/ldif/sort/SwingLdifSort.java
@@ -1,7 +1,6 @@
 package de.nanobyte.ldap.ldif.sort;
 
 import de.nanobyte.common.awt.GridBagConstraintsBuilder;
-import java.awt.Dimension;
 import static java.awt.GridBagConstraints.*;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -16,17 +15,16 @@ public class SwingLdifSort extends JFrame {
     private final static JList<Path> ldifFiles = new JList<>();
 
     static {
-        ldifFiles.setPreferredSize(new Dimension(300, 100));
-        ldifFiles.setVisibleRowCount(6);
+        ldifFiles.setFixedCellWidth(160);
     }
 
     public SwingLdifSort(final String[] arguments) {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Ldif Sort");
         this.setLayout(new GridBagLayout());
-        this.add(new JLabel("Ldif files"), new GridBagConstraintsBuilder().gridX(0).gridY(0).anchor(NORTHWEST).insets(new Insets(5, 5, 5, 0)).weightY(1).build());
-        this.add(ldifFiles, new GridBagConstraintsBuilder().gridX(0).gridY(1).anchor(NORTHWEST).insets(new Insets(0, 5, 5, 5)).fill(BOTH).weightY(1).weightX(1).build());
-        this.add(new JButton("Save"), new GridBagConstraintsBuilder().gridX(0).gridY(2).anchor(SOUTHEAST).insets(new Insets(0, 5, 5, 5)).build());
+        this.add(new JLabel("Ldif files"), new GridBagConstraintsBuilder().anchor(NORTHWEST).insets(new Insets(5, 5, 5, 0)).build());
+        this.add(ldifFiles, new GridBagConstraintsBuilder().gridX(0).anchor(NORTHWEST).insets(new Insets(0, 5, 5, 5)).fill(BOTH).weightY(1).weightX(1).build());
+        this.add(new JButton("Save"), new GridBagConstraintsBuilder().gridX(0).anchor(SOUTHEAST).insets(new Insets(0, 5, 5, 5)).build());
         this.pack();
     }
 }
