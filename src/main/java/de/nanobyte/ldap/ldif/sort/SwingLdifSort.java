ef4f502b5ca0ff153c8a01b1c795ba09ff6c97ca
Simon E. Silva Lauinger <simonsilvalauinger@users.noreply.github.com>
2013-05-23 17:56:59 +0200

    Just use NORTH, EAST, SOUTH and WEST to position the elements in the GridBagLayout cells.

--- src/main/java/de/nanobyte/ldap/ldif/sort/SwingLdifSort.java
+++ src/main/java/de/nanobyte/ldap/ldif/sort/SwingLdifSort.java
@@ -6,6 +6,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.nio.file.Path;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -22,9 +23,9 @@ public class SwingLdifSort extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Ldif Sort");
         this.setLayout(new GridBagLayout());
-        this.add(new JLabel("Ldif files"), new GridBagConstraintsBuilder().anchor(NORTHWEST).insets(new Insets(5, 5, 5, 0)).build());
-        this.add(ldifFiles, new GridBagConstraintsBuilder().gridX(0).anchor(NORTHWEST).insets(new Insets(0, 5, 5, 5)).fill(BOTH).weightY(1).weightX(1).build());
-        this.add(new JButton("Save"), new GridBagConstraintsBuilder().gridX(0).anchor(SOUTHEAST).insets(new Insets(0, 5, 5, 5)).build());
+        this.add(new JLabel("Ldif files"), new GridBagConstraintsBuilder().anchor(WEST).insets(new Insets(5, 5, 5, 0)).build());
+        this.add(ldifFiles, new GridBagConstraintsBuilder().gridX(0).anchor(WEST).insets(new Insets(0, 5, 5, 5)).fill(BOTH).weightY(1).weightX(1).build());
+        this.add(new JButton("Save"), new GridBagConstraintsBuilder().gridX(0).anchor(EAST).insets(new Insets(0, 5, 5, 5)).build());
         this.pack();
     }
 }
