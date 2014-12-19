cb49d384228d659cf5f0f5933a47fdfcdf8ef8e4
Simon E. Silva Lauinger <simon@silvalauinger.de>
2014-12-19 02:28:06 +0100

    LdifEntriesToStringCommand: 	- removed guava dependency. 	- using StrBuilder instead of StringBuilder.

--- ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/LdifEntriesToStringCommand.java
+++ ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/LdifEntriesToStringCommand.java
@@ -1,28 +1,26 @@
 package de.nanobyte.ldap.ldif.sort.cli.command;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import de.nanobyte.common.command.Command;
 import de.nanobyte.common.command.CommandException;
+import java.util.Collection;
+import static java.util.Objects.requireNonNull;
+import org.apache.commons.lang.text.StrBuilder;
 import org.apache.directory.api.ldap.model.ldif.LdifEntry;
 
 public class LdifEntriesToStringCommand implements Command<String> {
 
     //<editor-fold defaultstate="collapsed" desc="attributes">
-    private final Command<? extends Iterable<LdifEntry>> ldifEntriesCommand;
+    private final Command<? extends Collection<LdifEntry>> ldifEntriesCommand;
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="constructors">
-    public LdifEntriesToStringCommand(final Command<? extends Iterable<LdifEntry>> ldifEntriesCommand) {
-	this.ldifEntriesCommand = checkNotNull(ldifEntriesCommand);
+    public LdifEntriesToStringCommand(final Command<? extends Collection<LdifEntry>> ldifEntriesCommand) {
+	this.ldifEntriesCommand = requireNonNull(ldifEntriesCommand);
     }
     //</editor-fold>
 
     @Override
     public String execute() throws CommandException {
-	final StringBuilder concatenatedLdifEntries = new StringBuilder(1000);
-	for (final LdifEntry ldifEntry : ldifEntriesCommand.execute()) {
-	    concatenatedLdifEntries.append(ldifEntry);
-	}
-	return concatenatedLdifEntries.toString();
+        return new StrBuilder(1000).appendAll(ldifEntriesCommand.execute()).toString();
     }
 }
