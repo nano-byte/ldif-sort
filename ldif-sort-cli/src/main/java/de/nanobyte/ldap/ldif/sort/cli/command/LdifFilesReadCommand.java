31f329b1b2d72e29bcef871fa3478a9e146ea4b9
Simon E. Silva Lauinger <simon@silvalauinger.de>
2014-12-19 02:29:24 +0100

    LdifFilesReadCommand: removed guava dependency.

--- ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/LdifFilesReadCommand.java
+++ ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/LdifFilesReadCommand.java
@@ -1,12 +1,12 @@
 package de.nanobyte.ldap.ldif.sort.cli.command;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import de.nanobyte.common.command.Command;
 import de.nanobyte.common.command.CommandException;
 import de.nanobyte.ldap.ldif.sort.core.LdifFilesReader;
 import java.io.File;
 import java.util.Collection;
 import java.util.Iterator;
+import static java.util.Objects.requireNonNull;
 import org.apache.directory.api.ldap.model.ldif.LdapLdifException;
 import org.apache.directory.api.ldap.model.ldif.LdifEntry;
 
@@ -18,7 +18,7 @@ public class LdifFilesReadCommand implements Command<Iterable<LdifEntry>>, Itera
 
     //<editor-fold defaultstate="collapsed" desc="constructors">
     public LdifFilesReadCommand(final Collection<File> ldifFiles) {
-	this.ldifFiles = checkNotNull(ldifFiles);
+	this.ldifFiles = requireNonNull(ldifFiles);
     }
     //</editor-fold>
 
