f146b5fed40bc107176b1998df1e883755196a56
Simon E. Silva Lauinger <simon@silvalauinger.de>
2014-12-19 02:11:44 +0100

    HelpTextCommand: 	- removed guava dependency. 	- using StrBuilder to create message.

--- ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/HelpTextCommand.java
+++ ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/HelpTextCommand.java
@@ -1,8 +1,9 @@
 package de.nanobyte.ldap.ldif.sort.cli.command;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import com.martiansoftware.jsap.JSAP;
 import de.nanobyte.common.command.CommandException;
+import static java.util.Objects.requireNonNull;
+import org.apache.commons.lang.text.StrBuilder;
 
 public class HelpTextCommand extends UsageTextCommand {
 
@@ -12,14 +13,14 @@ public class HelpTextCommand extends UsageTextCommand {
 
     //<editor-fold defaultstate="collapsed" desc="constructors">
     public HelpTextCommand(final JSAP cliParser) {
-	super(cliParser);
-	this.helpText = super.execute() + System.lineSeparator()
-		+ checkNotNull(cliParser).getHelp();
+        super(cliParser);
+        this.helpText = new StrBuilder(super.execute()).appendNewLine()
+                .append(requireNonNull(cliParser).getHelp()).toString();
     }
     //</editor-fold>
 
     @Override
     public String execute() throws CommandException {
-	return helpText;
+        return helpText;
     }
 }
