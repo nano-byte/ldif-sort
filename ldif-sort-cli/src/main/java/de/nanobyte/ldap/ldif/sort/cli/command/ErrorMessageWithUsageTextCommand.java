fa1f39242a50eeb2fc53a4feb373c4fcc14c512f
Simon E. Silva Lauinger <simon@silvalauinger.de>
2014-12-19 02:10:13 +0100

    ErrorMessageWithUsageTextCommand: 	- removed guava dependency. 	- using StrBuilder to create message.

--- ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/ErrorMessageWithUsageTextCommand.java
+++ ldif-sort-cli/src/main/java/de/nanobyte/ldap/ldif/sort/cli/command/ErrorMessageWithUsageTextCommand.java
@@ -1,9 +1,10 @@
 package de.nanobyte.ldap.ldif.sort.cli.command;
 
-import static com.google.common.base.Preconditions.checkNotNull;
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPResult;
 import de.nanobyte.common.command.CommandException;
+import static java.util.Objects.requireNonNull;
+import org.apache.commons.lang.text.StrBuilder;
 
 public final class ErrorMessageWithUsageTextCommand extends UsageTextCommand {
 
@@ -13,15 +14,16 @@ public final class ErrorMessageWithUsageTextCommand extends UsageTextCommand {
 
     //<editor-fold defaultstate="collapsed" desc="constructors">
     public ErrorMessageWithUsageTextCommand(final JSAP cliParser, final JSAPResult parseResult) {
-	super(cliParser);
-	this.errorMessageWithHelpText = checkNotNull(parseResult).getErrorMessageIterator().next() + System.lineSeparator()
-		+ super.execute() + System.lineSeparator()
-		+ "Try --help for more information." + System.lineSeparator();
+        super(cliParser);
+        this.errorMessageWithHelpText = new StrBuilder()
+                .appendln(requireNonNull(parseResult).getErrorMessageIterator().next())
+                .appendln(super.execute())
+                .appendln("Try --help for more information.").toString();
     }
     //</editor-fold>
 
     @Override
     public String execute() throws CommandException {
-	return errorMessageWithHelpText;
+        return errorMessageWithHelpText;
     }
 }
